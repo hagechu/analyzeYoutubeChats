71224e9079cfbab2cdf2c8257790457a8130dde3
hagechu <hagetamachu@gmail.com>
2022-05-20 04:19:18 +0900

    firstCommit

---
+++ my-app/src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+import { GetTwitchData } from "./components/GetTwitchData";
+
+const App = () => {
+  return (
+    <div className="App">
+      <p>a</p>
+      <GetTwitchData />
+    </div>
+  );
+};
+
+export default App;
