759c38fb2bd6aa4087beb6ef7c6a5ee893a60659
hagechu <hagetamachu@gmail.com>
2022-05-28 08:52:39 +0900

    Add: フロントで入力したIDをサーバーに送るようにしました。 取得したデータをグラフにして画面に表示できるようにしました。

--- my-app/src/components/GetYoutubeChats.js
+++ my-app/src/components/GetYoutubeChats.js
@@ -1,22 +1,42 @@
 import React, { useState } from "react";
 import axios from "axios";
 
-export const GetYoutubeChats = () => {
-  const [chats, setChats] = useState([]);
+import { Graph } from "./Graph";
 
+export const GetYoutubeChats = () => {
+  const [chats, setChats] = useState([]); // チャットの全データを格納
+  const [videoID, setVideoID] = useState(""); // サーバーに送る動画のID
+  const [isLoading, setIsLoading] = useState(false); // リクエスト中の判別
+
+  //サーバーにポスト
   const postServer = () => {
-    axios.post("http://127.0.0.1:5000/getYoutubeChats").then((res) => {
-      setChats(res.data);
+    setIsLoading(true);
+    axios.post("http://127.0.0.1:5000/getYoutubeChats", { videoID: videoID }).then((response) => {
+      setChats(response.data);
+      setIsLoading(false);
     });
   };
 
-  console.log(chats.chats);
+  // URLからIDだけにする
+  const substrURL = (url) => {
+    setVideoID(url.target.value.substr(url.target.value.length - 11));
+  };
 
   return (
     <div>
+      <input value={videoID} onChange={substrURL} />
       <button onClick={postServer}>getChats</button>
+      {isLoading ? <p>分析中...</p> : <p>待機中</p>}
+      <p>testURL : https://www.youtube.com/watch?v=HFi7I-Z-86E</p>
+      <p>testURL : https://www.youtube.com/watch?v=dj2xt5wSA7A</p>
+      <p>testURL : https://www.youtube.com/watch?v=VH5z21x7_3c</p>
+      <Graph chats={chats.chats} />
     </div>
   );
 };
 
 // http://localhost:3000/
+
+// https://www.youtube.com/watch?v=HFi7I-Z-86E
+// https://www.youtube.com/watch?v=dj2xt5wSA7A
+// https://www.youtube.com/watch?v=VH5z21x7_3c
